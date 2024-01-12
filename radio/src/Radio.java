892c3793afa226fda752e346e092b22ecc792197
belennmm <belenmmonterroso@gmail.com>
2024-01-11 18:11:30 -0600

    Add existing file

---
+++ radio/src/Radio.java
@@ -0,0 +1,86 @@
+/**
+ * Representa la interface del programa.
+ */
+public class Radio implements IRadio{
+
+
+    private boolean encendido = false;
+    private String frecuencia;
+    private double estacion;
+    
+    
+    /** 
+     * @return boolean
+     */
+    @Override
+    public boolean isAM() {
+        if ( frecuencia.equals("AM")){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+
+    @Override
+    public boolean isOn() {
+        if (encendido == false){
+            return false;
+        }
+        else{
+            return true; 
+        }
+        
+    }
+
+    @Override
+    public double nextStation() {
+        
+        return 0;
+    }
+
+    @Override
+    public void saveStation(int buttonId, double station) {
+       
+        
+    }
+
+    @Override
+    public double selectStation(int buttonId) {
+      
+        return 0;
+    }
+
+    @Override
+    public void switchAMFM() {
+        if (frecuencia.equals("AM")){
+            frecuencia = "FM";
+        }
+
+        if (frecuencia.equals("FM")){
+            frecuencia = "AM";
+        }
+        
+    }
+
+    @Override
+    public void switchOnOff() {
+        if (isOn() == false){
+            encendido = true;
+            frecuencia = "AM";
+            estacion = 530.0;
+
+        }  
+        
+        if (isOn() == true){
+            encendido = false;
+        }
+
+        
+    }
+
+    
+    
+ 
+}
---
+++ radio/src/radioMain.java
@@ -0,0 +1,15 @@
+public class radioMain {
+    public static void main(String[] args) throws Exception {
+
+        Radio radio = new Radio();
+
+
+
+
+        System.out.println("----");
+
+        
+
+
+    }
+}
